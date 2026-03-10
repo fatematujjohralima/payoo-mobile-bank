7c258a6353fca33fb14ee8261fe793afb935e929
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-10 06:08:13 +0600

    handle login and redirect to the main page

--- script/login.js
+++ script/login.js
@@ -1,3 +1,15 @@
-document.getElementById('login-btn').addEventListener('click', function() {
-    
+document.getElementById('login-btn').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    const acNum=document.getElementById('acNum').value;
+    const acPin=document.getElementById('pin').value;
+    const pin=parseInt(acPin);
+
+    if(acNum === '123456789' && pin === 1234){
+        alert('Login successful!');
+        //window.location.href = 'dashboard.html';
+        window.location.href='main.html';
+    } else {
+        alert('Invalid account number or PIN. Please try again.');
+    }
+
 })
